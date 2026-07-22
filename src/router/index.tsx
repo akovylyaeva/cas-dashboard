4c7e643b7e498b53ceadfcf440964df9b7eb6e0d
akovylyaeva <dazaj.osamu@mail.ru>
2026-07-22 22:05:15 +0500

    feat: add base layout

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom"
+import { Layout } from "../layout/Layout"
 import { Dashboard } from "../pages/dashboard/Dashboard"
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+    ],
   },
 ])
