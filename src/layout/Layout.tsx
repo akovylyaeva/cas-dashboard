bbcf74d74dfeacaaa53baa7d25a1ba83c339b03c
akovylyaeva <dazaj.osamu@mail.ru>
2026-07-22 22:23:21 +0500

    feat: add base header

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom"
 import { Sidebar } from "../components/sidebar/Sidebar"
+import { Header } from "../components/header/Header"
 
 export function Layout() {
   return (
     <div>
       <Sidebar />
 
-      <main>
-        <Outlet />
-      </main>
+      <div>
+        <Header />
+
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
